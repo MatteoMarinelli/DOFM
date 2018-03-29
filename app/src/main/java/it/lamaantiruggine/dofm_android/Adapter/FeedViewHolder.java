36385a057ffe2b5ad64fa106714f41ae2f62cfd0
Matteo Marinelli <theaster@protonmail.com>
2018-03-29 11:43:05 +0200

    j

---
+++ app/src/main/java/it/lamaantiruggine/dofm_android/Adapter/FeedViewHolder.java
@@ -0,0 +1,54 @@
+package it.lamaantiruggine.dofm_android.Adapter;
+
+
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import it.lamaantiruggine.dofm_android.Interface.ItemClickListener;
+import it.lamaantiruggine.dofm_android.Model.RSSObject;
+import it.lamaantiruggine.dofm_android.R;
+import it.lamaantiruggine.dofm_android.Webviewer;
+
+class FeedViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,View.OnLongClickListener {
+
+    private RSSObject rssObject;
+    public TextView txtTitle, txtPubDate, txtContent, txtCategory;
+    public String item_url;
+    private ItemClickListener itemClickListener;
+    private Intent i = new Intent(itemView.getContext(), Webviewer.class);
+
+    public FeedViewHolder (View itemView) {
+        super(itemView);
+        txtTitle = (TextView)itemView.findViewById(R.id.txtTitle);
+        txtPubDate = (TextView)itemView.findViewById(R.id.txtPubDate);
+        // txtContent = (TextView)itemView.findViewById(R.id.txtContent);
+        txtCategory = (TextView)itemView.findViewById(R.id.txtCategory);
+
+        itemView.setOnClickListener(this);
+        itemView.setOnLongClickListener(this);
+    }
+
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        String url = FeedAdapter.url_item.toString();
+        i.putExtra("LINK", url);
+        itemView.getContext().startActivity(i);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String url = FeedAdapter.url_item.toString();
+        i.putExtra("LINK", url);
+        itemView.getContext().startActivity(i);
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        String url = FeedAdapter.url_item.toString();
+        i.putExtra("LINK", url);
+        itemView.getContext().startActivity(i);
+        return true;
+    }
+}
---
+++ app/src/main/java/it/lamaantiruggine/dofm_android/Webviewer.java
@@ -0,0 +1,20 @@
+package it.lamaantiruggine.dofm_android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class Webviewer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview_layout);
+
+        Intent intent = getIntent();
+        String url = intent.getExtras().getString("LINK");
+        WebView wv=(WebView)findViewById(R.id.item_webview);
+        wv.loadUrl(url);
+    }
+}
